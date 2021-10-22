3c2f5c23ffd29a687e013e92a3990bdc6bafe986
Chayanit McCormick <72841865+chay-chay@users.noreply.github.com>
2021-10-22 13:41:06 -0400

    Added Fontawesome

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,14 +1,20 @@
 <template>
-    <div v-for="task in tasks">
-        <h3>{{ task.text }}</h3>
+    <div :key="task.id" v-for="task in tasks">
+        <Task :task="task" />
+        <!-- <h3>{{ task.text }}</h3> -->
     </div>
 </template>
 
 <script>
+import Task from './Task'
+
     export default {
         name: 'Tasks',
         props: {
             tasks: Array
+        },
+        components: {
+            Task
         }
     }
 </script>
