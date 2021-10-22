33dd36d0cf7aa0bbd4028d505dec95f5b8e65e6a
Chayanit McCormick <72841865+chay-chay@users.noreply.github.com>
2021-10-22 11:19:04 -0400

    Added Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,13 @@
+<template>
+  <button v-on:click="onClick()" :style="{ background: color }" class="btn">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+};
+</script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,23 +1,30 @@
 <template>
-    <header>
-        <h1>{{ title }}</h1>
-    </header>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green" />
+    <!-- <Button text="Update Task" color="blue" />
+    <Button text="Delete Task" color="red" /> -->
+  </header>
 </template>
 
 <script>
+import Button from "./Button.vue";
 export default {
-    name: 'Header',
-    props: {
-        title: String,
-    }, // or can define as obj { title:  String} or like this  props: ['title']
-}
+  name: "Header",
+  props: {
+    title: String,
+  }, // or can define as obj { title:  String} or like this  props: ['title']
+  components: {
+    Button,
+  },
+};
 </script>
 
 <style scoped>
 header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
 }
-    </style>
+</style>
