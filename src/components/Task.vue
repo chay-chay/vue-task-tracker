1f79edc983d6f30e8cc17045b1e0e6ff27ef96f1
Chayanit McCormick <72841865+chay-chay@users.noreply.github.com>
2021-10-22 13:43:49 -0400

    Dynamic class

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,15 +1,14 @@
 <template>
-  <div class="task">
-    <h3>{{ task.text }} 
-          <i class="fas fa-times"></i>
+  <div :class="[task.reminder? 'reminder' : '' , 'task']">
+    <h3>
+      {{ task.text }}
+      <i class="fas fa-times"></i>
     </h3>
     <p>{{ task.day }}</p>
-  
   </div>
 </template>
 
 <script>
-
 export default {
   name: "Task",
   props: {
